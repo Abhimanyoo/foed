be0145797f154b31868ad51bb8d98d1acca08b85
Kees Kluskens <kees@webduck.nl>
2018-05-11 15:05:43 +0200

    Fix Now deployment (at least try to)

--- menu-app/pages/_app.tsx
+++ menu-app/pages/_app.tsx
@@ -1,6 +1,8 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 
+// Not really necessary at the moment but it will be.
+// TODO fix types
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
