5a42fae989d8bfd4201419a7df58fc1547673dea
Kees Kluskens <kees@webduck.nl>
2018-05-18 15:34:58 +0200

    Fix cache not refreshing
    Fixes #33

--- menu-app/withApollo.ts
+++ menu-app/withApollo.ts
@@ -1,6 +1,17 @@
 import nWithApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const uri = process.env.REACT_APP_GRAPHQL_URL;
 
 export const withApollo = nWithApollo({
-  client: new ApolloClient({ uri: process.env.REACT_APP_GRAPHQL_URL }),
+  client: () =>
+    ({
+      cache: new InMemoryCache(),
+    } as any), // workaround for https://github.com/lfades/next-with-apollo/issues/12
+  link: {
+    http: new HttpLink({
+      uri,
+    }),
+  },
 });
