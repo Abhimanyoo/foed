24a0fff872337a9e36fbcc9ae60880c7d2566d97
Kees Kluskens <kees@webduck.nl>
2018-05-31 16:05:17 +0200

    Start experimenting with mobx

--- menu-app/pages/_app.tsx
+++ menu-app/pages/_app.tsx
@@ -1,4 +1,35 @@
-import { ApolloApp } from 'next-with-apollo';
+import React from 'react';
+import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
 import { withApollo } from '../withApollo';
+import { initStore, Store } from '../Store';
+import { WithApolloProps } from 'next-with-apollo';
 
-export default withApollo(ApolloApp);
+interface Props extends WithApolloProps<any> {
+  Component: any;
+  pageProps: object;
+}
+
+class MyApp extends App {
+  props: Props;
+  store: Store;
+
+  constructor(props: Props) {
+    super(props);
+    this.store = initStore();
+  }
+
+  public render() {
+    const { Component, pageProps, apollo } = this.props;
+
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Component {...pageProps} store={this.store} />
+        </ApolloProvider>
+      </Container>
+    );
+  }
+}
+
+export default withApollo(MyApp);
--- menu-app/withApollo.ts
+++ menu-app/withApollo.ts
@@ -1,17 +1,7 @@
 import nWithApollo from 'next-with-apollo';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import ApolloClient from 'apollo-boost';
 
 const uri = process.env.REACT_APP_GRAPHQL_URL;
 
-export const withApollo = nWithApollo({
-  client: () =>
-    ({
-      cache: new InMemoryCache(),
-    } as any), // workaround for https://github.com/lfades/next-with-apollo/issues/12
-  link: {
-    http: new HttpLink({
-      uri,
-    }),
-  },
-});
+// TODO: typings appear to be wrong, maybe send PR to fix.
+export const withApollo = (nWithApollo as any)(() => new ApolloClient({ uri }));
