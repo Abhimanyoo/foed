864a12dfd0c1d7503bc6bf07dea7d830b96463a6
Rutger <info@rutgerschimmel.nl>
2018-08-07 12:14:16 +0200

    Fix login tablet layout

--- restaurant/component/Login.tsx
+++ restaurant/component/Login.tsx
@@ -12,17 +12,16 @@ export const LoginForm = styled('form')`
   background: #4192a3;
   padding: 20px;
   border-radius: 5px;
-  box-shadow: 0 4px 10px 0 rgba(12,70,81,0.36);
+  box-shadow: 0 4px 10px 0 rgba(12, 70, 81, 0.36);
 `;
 
 export const LoginContainer = styled('div')`
-  width: 300px;
-  margin: 0 20px 0 250px;
+  width: 500px;
   text-align: center;
+  padding: 80px 40px;
   color: #fff;
   @media (max-width: 550px) {
     margin: 0 auto;
-    padding: 80px 40px;
     width: 100%;
   }
 `;
