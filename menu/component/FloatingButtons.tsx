5e25abefe67f1f7e4cad776466864c6d3fdd888e
Rutger <info@rutgerschimmel.nl>
2018-09-11 11:09:40 +0200

    Menu: improve floating gradient

--- menu/component/FloatingButtons.tsx
+++ menu/component/FloatingButtons.tsx
@@ -10,13 +10,13 @@ const floatingStyles = css`
   position: fixed;
   z-index: 1;
   bottom: 0;
-  padding: 0 12px 20px 20px;
+  padding: 0 12px 0 20px;
   display: flex;
   justify-content: center;
   background: linear-gradient(
     to bottom,
-    rgba(66, 146, 163, 0) 0%,
-    rgba(66, 146, 163, 1) 100%
+    rgba(3, 152, 171, 0) 0%,
+    rgba(3, 152, 171, 1) 100%
   );
 `;
 
